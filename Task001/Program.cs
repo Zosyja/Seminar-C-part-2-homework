25e0655be119209655ac29702c1cbd1157aa3183
Zosyja <zosyja@bk.ru>
2023-01-21 20:52:27 +0300

    added Task001

---
+++ Task001/Program.cs
@@ -0,0 +1,15 @@
+﻿// Дано число обозначающее день недели. Выяснить является номер дня недели выходным
+Console.Write("Введите число от 1 до 7: ");
+int day = int.Parse(Console.ReadLine() ?? "0");
+switch (day)
+{
+    case 1: Console.WriteLine("Понедельник"); break;
+    case 2: Console.WriteLine("Вторник"); break;
+    case 3: Console.WriteLine("Среда"); break;
+    case 4: Console.WriteLine("Четверг"); break;
+    case 5: Console.WriteLine("Пятница"); break;
+    case 6: Console.WriteLine("Суббота"); break;
+    case 7: Console.WriteLine("Воскресенье"); break;
+}
+if (day == 6 || day == 7) Console.WriteLine("Выходной");
+else Console.WriteLine("Рабочий");
